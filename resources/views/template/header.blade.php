dc0292b43cca4ccf1b756f823e4226ab58f5e694
riiny <riinvalena1@gmail.com>
2024-10-17 18:54:26 +0800

    feat: added logo to header, changed header button styles

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -1,19 +1,26 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
+    <div class="container-fluid flex-column align-items-center">
+        <!-- Navbar Brand - Centered Above the Collapse -->
+        <a class="navbar-brand" href="/">
+            <img src="https://i.ibb.co/XydNNvZ/Logo-Maker-Ca-1728976782332-Copy.png" alt="Logo" style="height: 100px;">
+        </a>
 
-        <a class="navbar-brand" href="/">Navbar w/ text</a>
-
+        <!-- Navbar Toggler (Visible on smaller screens) -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
-      
+        
+        <!-- Navbar Collapse -->
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Home</a>
+                    <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Events</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::is('about') ? 'active' : '' }}" href="/about">About</a>
+                    <a class="nav-link {{ Request::is('about') ? 'active' : '' }}" href="/about">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('about') ? 'active' : '' }}" href="/about">Publications</a>
                 </li>
 
                 {{-- checks if user is logged in or not --}}
@@ -29,12 +36,9 @@
                         <a class="nav-link {{ Request::is('login') ? 'active' : '' }}" href="/login">Login</a>
                     </li>
                 @endif
-                
             </ul>
 
-            <span class="navbar-text">
-                Navbar text with an inline element
-            </span>
+            
         </div>
     </div>
-  </nav>
+</nav>
