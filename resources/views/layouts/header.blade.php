320d4f64246b30752078664a35622eaf86496bd2
riiny <riinvalena1@gmail.com>
2024-11-28 16:31:30 +0800

    fix: updated logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,10 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid flex-column align-items-center">
         <!-- Navbar Brand - Centered Above the Collapse -->
-        <a class="navbar-brand" href="/">
-    <img src="https://i.ibb.co/Ch6Cvzb/logo-horizontal.png" alt="Logo" style="height: 130px;">
-</a>
-
+        <a class="navbar-brand" href="/"> <img src="{{ asset('assets/images/logo_horizontal.png') }}" alt="Logo" style="height: 130px;"> </a>
 
         <!-- Navbar Toggler (Visible on smaller screens) -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,29 +10,26 @@
         
         <!-- Navbar Collapse -->
         <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                
+            <ul class="navbar-nav mb-2 mb-lg-0">
                 {{-- checks if user is logged in or not --}}
                 @if (Auth::check())
 
                     {{-- checks if the user role is admin --}}
                     @if (Auth::user()->role == 'admin')
-                        <li class="nav-item">
-                            <a class="nav-link {{ Request::is('admin') ? 'active' : '' }}" href="/admin">Dashboard</a>
-                        </li>
+                        <!-- Add admin-specific links here -->
                     @endif
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="/logout">Logout</a>
-                    </li>
-                @else
-                    <li class="nav-item">
-                        <a class="nav-link {{ Request::is('login') ? 'active' : '' }}" href="/login">Login</a>
-                    </li>
                 @endif
             </ul>
 
-            
+            <!-- Align logout to the right -->
+            @if (Auth::check())
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Log out</a>
+                    </li>
+                </ul>
+            @endif
         </div>
     </div>
 </nav>
