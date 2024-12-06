2a2c90081d69372b7f33ec6ef1428cba8fa464f3
riiny <riinvalena1@gmail.com>
2024-12-07 02:17:27 +0800

    feat: jansen help pelase

--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -11,7 +11,7 @@
 
     <div class="row">
         <!-- Middle Section: Messages -->
-        <div class="col-md-9"> <!-- Changed to col-md-9 -->
+        <div class="col-md-9"> <!-- Middle section -->
             <div class="card mb-4">
                 <div class="card-header">
                     <h4>{{ isset($groupChat) ? $groupChat->name : 'No Group Selected' }}</h4>
@@ -49,20 +49,45 @@
             </div>
         </div>
 
-        <!-- Right Section: User Info and Progress -->
-        <div class="col-md-3"> <!-- Right section remains col-md-3 -->
-            <div class="card mb-4 text-center">
-                <div class="card-body">
-                    <div class="avatar bg-secondary rounded-circle mx-auto mb-3" style="width: 100px; height: 100px;"></div>
-                    <h6>Overall Progress</h6>
-                    <div class="progress mb-3">
-                        <div class="progress-bar bg-primary" style="width: {{ isset($progress) ? $progress : 0 }}%;" aria-valuenow="{{ isset($progress) ? $progress : 0 }}" aria-valuemin="0" aria-valuemax="100">
-                            {{ isset($progress) ? $progress : 0 }}%
-                        </div>
-                    </div>
+        <!-- Right Section: Members -->
+        <div class="col-md-3"> <!-- Right section -->
+            <h5>Members:</h5>
+            <div class="card">
+                <div class="card-header" id="membersHeading" role="button" data-bs-toggle="collapse" data-bs-target="#membersList" aria-expanded="false" aria-controls="membersList" style="cursor: pointer;">
+                    <h5 class="mb-0 d-flex justify-content-between align-items-center">
+                        Members ({{ isset($members) ? $members->count() : 0 }})
+                        <i class="fas fa-chevron-down toggle-icon"></i>
+                    </h5>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+            
+                <div id="membersList" class="collapse" aria-labelledby="membersHeading">
+                    <div class="card-body">
+                        @if(isset($members) && $members->count() > 0)
+                            <ul class="list-group">
+                                @foreach ($members as $member)
+                                    <li class="list-group-item d-flex align-items-center list-group-members">
+                                        <img 
+                                            src="{{ asset('storage/profile/' . $member->picture) }}" 
+                                            alt="{{ $member->first_name .' ' . $member->last_name }}" 
+                                            class="rounded-circle me-3" 
+                                            style="width: 50px; height: 50px; object-fit: cover;"
+                                        >
+                                        <div class="d-flex flex-column align-items-start">
+                                            <h6 class="mb-1">{{ $member->first_name . ' ' . $member->last_name }}</h6>
+                                            <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to={{ $member->email }}" 
+                                               class="text-muted small text-decoration-none" 
+                                               title="Send email to {{ $member->first_name }}"
+                                               target="_blank">
+                                                {{ $member->email }}
+                                                <i class="fas fa-envelope ms-2 text-primary" style="font-size: 0.8rem;"></i>
+                                            </a>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @else
+                            <div class="alert alert-info">No members found.</div>
+                        @endif
+                        </div>
+                        @endsection
+                
